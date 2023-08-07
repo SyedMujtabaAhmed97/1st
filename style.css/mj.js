f266f5ad0b504b03db9d22f9728b0016551125fd
m.yashalhasan <yashalking109@gmail.com>
2023-08-07 18:16:22 +0500

    first commit

---
+++ style.css/mj.js
@@ -0,0 +1,10 @@
+
+
+var num1 = document.getElementById("inp1");
+var num2 = document.getElementById("inp2");
+var num3 = document.getElementById("pi");
+function result() {
+
+    num3.innerHTML=parseInt(num1.value) + parseInt(num2.value)
+    
+}
